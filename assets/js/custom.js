1ce6c499e7a2a2c8528d0af7854b568dfa9babde
Onup Chandra Barmon <43741354+onuproy@users.noreply.github.com>
2025-01-11 23:30:54 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,34 @@
-jQuery(document).ready(function(){ 
+jQuery(document).ready(function () {
 
 	// Mobile menu 
 	jQuery('#toggle').click(function () {
 		jQuery(this).toggleClass('active');
 		jQuery('#overlay').toggleClass('open');
+	});
+
+	// Home owlCarousel
+	jQuery('.our_work_slider_area').owlCarousel({
+		loop: true,
+		margin: 25,
+		nav: true,
+		autoplay: false,
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		dots: false,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			576: {
+				items: 2,
+
+			},
+			768: {
+				items: 2,
+			},
+			1200: {
+				items: 3,
+			}
+		}
 	});
 
 
